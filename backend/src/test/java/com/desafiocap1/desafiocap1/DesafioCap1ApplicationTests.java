8f47635eef5e19f8ea6aec1efae0652e29b6dc9d
Fabiano Brancher <fabiano.brancher@hotmail.com>
2021-10-23 16:30:04 -0300

    Spring initializr

---
+++ backend/src/test/java/com/desafiocap1/desafiocap1/DesafioCap1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiocap1.desafiocap1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCap1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
