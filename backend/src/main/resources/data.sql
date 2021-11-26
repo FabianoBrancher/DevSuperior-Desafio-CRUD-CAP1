ac095f640b78d49611f3f69f07d770eb86cef866
Fabiano Brancher <fabiano.brancher@hotmail.com>
2021-11-25 23:29:28 -0300

    seed

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,2 +1,40 @@
 INSERT INTO CLIENT (id, birth_date, children, cpf, income, name)
-VALUES (1, '2020-07-14T10:00:00Z', 1, '042.864.519-41', 3000, 'FABIANO BRANCHER');
+VALUES (1, '2020-07-14T10:00:00Z', 1, '111.344.229-41', 10000, 'Paulo Muzy');
+INSERT INTO CLIENT (id, birth_date, children, cpf, income, name)
+VALUES (2, '2020-07-14T10:00:00Z', 2, '342.863.159-78', 2000, 'Diego Castanho de Brito');
+INSERT INTO CLIENT (id, birth_date, children, cpf, income, name)
+VALUES (3, '2020-07-14T10:00:00Z', 0, '683.124.419-41', 3000, 'Agnaldo Barbieri');
+INSERT INTO CLIENT (id, birth_date, children, cpf, income, name)
+VALUES (4, '2020-07-14T10:00:00Z', 3, '042.845.559-45', 4000, 'Marcio Muniz de Souza');
+INSERT INTO CLIENT (id, birth_date, children, cpf, income, name)
+VALUES (5, '2020-07-14T10:00:00Z', 0, '542.845.912-31', 3500, 'Tatiele Mendonça');
+INSERT INTO CLIENT (id, birth_date, children, cpf, income, name)
+VALUES (6, '2020-07-14T10:00:00Z', 1, '642.564.591-51', 2700, 'Fabrízio Digirolamo');
+INSERT INTO CLIENT (id, birth_date, children, cpf, income, name)
+VALUES (7, '2020-07-14T10:00:00Z', 2, '221.364.519-44', 1500, 'Ellen Adarna');
+INSERT INTO CLIENT (id, birth_date, children, cpf, income, name)
+VALUES (8, '2020-07-14T10:00:00Z', 3, '009.224.192-66', 5000, 'Camila Capello Dias');
+INSERT INTO CLIENT (id, birth_date, children, cpf, income, name)
+VALUES (9, '2020-07-14T10:00:00Z', 0, '024.688.919-01', 7000, 'Ricardo Amorim');
+INSERT INTO CLIENT (id, birth_date, children, cpf, income, name)
+VALUES (10, '2020-07-14T10:00:00Z', 5, '427.841.159-12', 2900, 'Jefferson Fernandez');
+INSERT INTO CLIENT (id, birth_date, children, cpf, income, name)
+VALUES (11, '2020-07-14T10:00:00Z', 3, '023.864.519-31', 5500, 'James Bond Junior');
+INSERT INTO CLIENT (id, birth_date, children, cpf, income, name)
+VALUES (12, '2020-07-14T10:00:00Z', 4, '321.864.519-41', 3300, 'Ozélio Prado da Costa');
+INSERT INTO CLIENT (id, birth_date, children, cpf, income, name)
+VALUES (13, '2020-07-14T10:00:00Z', 1, '123.800.509-41', 3100, 'Nadir Gonçalves');
+INSERT INTO CLIENT (id, birth_date, children, cpf, income, name)
+VALUES (14, '2020-07-14T10:00:00Z', 1, '452.466.129-41', 2000, 'Emanuel Ferreira dos Santos');
+INSERT INTO CLIENT (id, birth_date, children, cpf, income, name)
+VALUES (15, '2020-07-14T10:00:00Z', 0, '677.368.359-41', 2200, 'Edivaldo Junior Tarteli');
+INSERT INTO CLIENT (id, birth_date, children, cpf, income, name)
+VALUES (16, '2020-07-14T10:00:00Z', 1, '123.260.519-41', 2400, 'Fernanda Bittencourt');
+INSERT INTO CLIENT (id, birth_date, children, cpf, income, name)
+VALUES (17, '2020-07-14T10:00:00Z', 4, '777.994.669-41', 5600, 'Sonia Abreu Tertuliano');
+INSERT INTO CLIENT (id, birth_date, children, cpf, income, name)
+VALUES (18, '2020-07-14T10:00:00Z', 1, '098.324.123-41', 6500, 'Carlos Francisco da Silva');
+INSERT INTO CLIENT (id, birth_date, children, cpf, income, name)
+VALUES (19, '2020-07-14T10:00:00Z', 0, '022.464.779-77', 2100, 'Marcelo Jacometi');
+INSERT INTO CLIENT (id, birth_date, children, cpf, income, name)
+VALUES (20, '2020-07-14T10:00:00Z', 2, '202.885.559-55', 3000, 'Ulisses Baroni');
